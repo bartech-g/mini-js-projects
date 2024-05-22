9df2ca021e932091383feb87807445b716bcc7fb
bartech-g <bartech.js@gmail.com>
2024-05-22 16:51:18 +0200

    master folder

---
+++ functions/script.js
@@ -0,0 +1,14 @@
+// function makeFunc() {
+//     const name = "Mozilla";
+//     function displayName() {
+//       console.log(name);
+//     }
+//     return displayName;
+//   }
+  
+//   const myFunc = makeFunc();
+
+const fruits = ['apple', 'banana', 'orange', 'grape', 'strawberry', 'kiwi', 'watermelon', 'pineapple', 'mango', 'pear'];
+const fruits2 = [...fruits, "new item"]
+
+console.log(fruits2)
